a4ce85ce85988ddaa0bb55e2f571c8fd5d579464
Dafydd Thomas <jingovimes@gmail.com>
2015-11-16 23:04:31 +0000

    started building server functionality

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+CollectionDriver = require('./private/collectionDriver').CollectionDriver;
+
+/**
+ * Set the port number to 3000 but allow it to get overriden in the command
+ * line.
+ */
+app.set('port', process.env.PORT || 3000);
+
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
-console.log('server listeing on port 8080');
+/**
+ * Catch all function which accepts all arguments and will return a 404.
+ */
+app.use(function (req,res) { //1
+    res.render('404', {url:req.url}); //2
+});
+
+app.listen(app.get('port'));
+console.log('server listeing on port '  + app.get('port'));
